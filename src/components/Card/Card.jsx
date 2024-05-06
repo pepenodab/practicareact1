ca11d2cacdbee54213b9b4b217ac9cd611106c70
___pepe_ <usr3854@salesianos-lacuesta.net>
2024-05-06 21:04:37 +0100

    adri es mi perrita

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,78 +1,42 @@
 import PropTypes from 'prop-types';
 import './Card.css';
 
+const changerFormat = ({ dateTime }) => {
+  const date = new Date(dateTime).toLocaleDateString('es-ES', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  return date;
+};
+
 const Card = ({ character }) => {
-  const characters = [
-    {
-      id: 1,
-      name: 'Rick Sanchez',
-      status: 'Alive',
-      species: 'Human',
-      gender: 'Male',
-      origin: 'Earth (C-137)',
-      location: 'Citadel of Ricks',
-      created: '2017-11-04T18:48:46.250Z'
-    },
-    {
-      id: 2,
-      name: 'Morty Smith',
-      status: 'Alive',
-      species: 'Human',
-      gender: 'Male',
-      origin: 'unknown',
-      location: 'Citadel of Ricks',
-      created: '2017-11-04T18:50:21.651Z'
-    },
-    {
-      id: 3,
-      name: 'Summer Smith',
-      status: 'Alive',
-      species: 'Human',
-      gender: 'Female',
-      origin: 'Earth (Replacement Dimension)',
-      location: 'Earth (Replacement Dimension)',
-      created: '2017-11-04T19:09:56.428Z'
-    },
-    {
-      id: 4,
-      name: 'Beth Smith',
-      status: 'Alive',
-      species: 'Human',
-      gender: 'Female',
-      origin: 'Earth (Replacement Dimension)',
-      location: 'Earth (Replacement Dimension)',
-      created: '2017-11-04T19:22:43.665Z'
-    },
-    {
-      id: 6,
-      name: 'Abadango Cluster Princess',
-      status: 'Alive',
-      species: 'Human',
-      gender: 'Male',
-      origin: 'Abadango',
-      location: 'Abadango',
-      created: '2017-11-04T19:50:28.250Z'
-    },
-    {
-      id: 13,
-      name: 'Alien Googah',
-      status: 'Dead',
-      species: 'Alien',
-      gender: 'Male',
-      origin: 'unknown',
-      location: 'Earth (Replacement Dimension)',
-      created: '2017-11-04T20:33:30.779Z'
-    }
-  ];
-  if (character == characters.name) {
-    return (
-      <>
-        <div></div>
-      </>
-    );
-  } else {
-    return;
-  }
+  return (
+    <>
+      <div className="profile">
+        <div className="container">
+          <div className="profile__wrapper">
+            <div className="profile__avatar">
+              <img src={`src/images/${character.id}.jpeg`} alt="" />
+            </div>
+            <div className="profile__h3">
+              <h3>{character.name}</h3>
+            </div>
+            <div className="profile__status">
+              <p>{character.status}</p>
+            </div>
+            <div className="profile__learnmore">
+              <p>{character.species}</p>
+              <p>{character.gender}</p>
+              <p>{character.location}</p>
+              <p>{changerFormat(character.created)}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 Card.propTypes = {
